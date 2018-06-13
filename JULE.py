996a53bc10f986a30b5534675e24ec283b800de1
Joris Guerin <joris.guerin@ensam.eu>
2018-06-13 16:46:59 +0200

    cluster initialization working, added verbose

--- JULE.py
+++ JULE.py
@@ -5,13 +5,14 @@ from utils.agglomerative_clustering import *
 from termcolor import colored
 
 import numpy as np
+import time
 
 class JULE:
 
 	def __init__(self, datafile, modelfile, params):
 
 		self.features_orig, self.true_labels = load_data(datafile)
-		print(colored("\nLoaded dataset of shape", "green"), self.features_orig.shape)
+		print(colored("\nLoaded dataset of shape", "blue"), self.features_orig.shape)
 
 		self.n_samples = self.features_orig.shape[0]
 
@@ -20,7 +21,9 @@ class JULE:
 		self.initialize_clusters()
 
 	def initialize_clusters(self):
-		self.features_current = self.features_orig
-		_, indices = get_NearestNeighbors(self.features_current, self.params['k_neighbors_pts'])
-
+		start = time.time()
+		_, indices = get_NearestNeighbors(self.features_orig, self.params['k_neighbors_pts'])
 		self.labels_current_table = initialize_clusters(indices, self.n_samples)
+		print(colored("\nInitialized clusters", "blue")
+		print(colored("N clusters: %d" % len(self.labels_current_table), "green")
+		print(colored("Time: %f" % time.time() - start, "purple"))
